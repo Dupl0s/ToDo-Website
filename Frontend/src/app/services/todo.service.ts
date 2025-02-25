859922e64da946944821dbf821dcfa53f03ff4fe
JojoArchiv <johanna.wolfstieg@posteo.de>
2025-02-25 11:44:23 +0100

    List View of Todos
    Todos are in a Table View on the route /todos. Examples are in a json file.

---
+++ Frontend/src/app/services/todo.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Todo } from '../model/todo.type';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+  http = inject(HttpClient);
+
+  getTodosFromApi(): Observable<Todo[]> {
+    const url = '../assets/todos.json';
+    console.log('Lade JSON von:', url);
+    return this.http.get<Todo[]>(url);
+/*     return this.http.get<Array<Todo>>(url);
+ */  }
+}
---
+++ Frontend/src/app/todos/todos.component.ts
@@ -0,0 +1,26 @@
+import { Component, inject, signal } from '@angular/core';
+import { TodoService } from '../services/todo.service';
+import { Todo } from '../model/todo.type';
+import { map } from 'rxjs';
+
+@Component({
+  selector: 'app-todos',
+  standalone: true,
+  imports: [],
+  templateUrl: './todos.component.html',
+  styleUrl: './todos.component.css'
+})
+export class TodosComponent {
+  todos = signal<Array<Todo>>([])
+  todoService = inject(TodoService);
+
+  ngOnInit() {
+    this.todoService.getTodosFromApi()
+    .pipe(
+      map(data => data.slice(0, 6))
+    )
+    .subscribe((data) => {
+      this.todos.set(data);
+    });
+  }
+}
