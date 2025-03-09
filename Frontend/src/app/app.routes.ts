79aa95f6237e4f5e3755ef3f09d1943fcb70727e
Sribriddhi <sribriddhibanerjee@gmail.com>
2025-03-09 19:48:39 +0100

    Revert "Papierkorb and Homepage"
    This reverts commit 3f7731d9df987818ba69c87dbc8f967518181187.

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,11 +1,3 @@
-import {Routes} from '@angular/router';
+import { Routes } from '@angular/router';
 
-
-export const routes: Routes = [
-    {path: 'todos', loadComponent: () =>  {return import('./todos/todos.component').then((m) => m.TodosComponent)
-    }},
-    {path: 'dustbin', loadComponent: () => {return import('./dustbin/dustbin.component').then((m) => m.DustbinComponent)
-    }},
-    {path: '', loadComponent: () =>{return import('./home/home.component').then((m) => m.HomeComponent)
-    }}
-];
+export const routes: Routes = [];
