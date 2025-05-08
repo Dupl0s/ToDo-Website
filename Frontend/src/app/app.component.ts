dbd80a19d98aab991bda60a1a9f3f818fdf51f55
Jannik Haase <jannik.haase@sick.de>
2025-05-08 11:55:46 +0200

    new header style + functional via RouterLink

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,13 +1,38 @@
 import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
+import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterModule, RouterOutlet } from "@angular/router";
+import { filter } from "rxjs/operators";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterModule],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css"
 })
 export class AppComponent {
-  title = "TINF24B5";
+  title = "ToDo Website";
+  urlName: string | null = '';
+  constructor(private router: Router, private route: ActivatedRoute) {}
+  ngOnInit(): void {
+    // Auf jede Navigation hören
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        const currentUrl = this.router.url;
+        const segments = currentUrl.split('/');
+        const secondSegement = segments[1];
+        switch (secondSegement) {
+          case 'todos':
+            this.urlName = 'To-Do`s';
+            break;
+         //Beispiel
+         // case '[url]':
+         // this.urlName = '[shown text]';
+         // break;
+          default:
+            this.urlName = 'Home';
+            break;
+        }
+      });
+  }
 }
