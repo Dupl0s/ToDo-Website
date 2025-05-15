eca024f2408c952b39e3102fa181c958856f3ec6
Duplo <Blitzcolt34@gmail.com>
2025-05-15 19:11:06 +0200

    added path of todos.json + possible error fix

--- Frontend/src/app/services/todo.service.ts
+++ Frontend/src/app/services/todo.service.ts
@@ -2,24 +2,48 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { Todo } from '../model/todo.type';
 import { map, Observable } from 'rxjs';
-import todoData from 'C:/Users/haaseja/Desktop/DHBW/Bericht Repo/ToDo-Website/Frontend/src/assets/todos.json'
-
+import todoData from '../../assets/todos.json';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoService {
   http = inject(HttpClient);
 
   localTodos: Todo[] = [
-    { id: 7, userId: 1, title: 'Beispiel Todo 1', completed: false, deadline: '2025-10-15', niveau: 3, importance: 5 },
-    { id: 8, userId: 1, title: 'Beispiel Todo 2', completed: true, deadline: '2025-11-20', niveau: 2, importance: 4 },
-    { id: 9, userId: 1, title: 'Beispiel Todo 3', completed: true, deadline: '2025-11-20', niveau: 2, importance: 4 }
+    {
+      id: 7,
+      userId: 1,
+      title: 'Beispiel Todo 1',
+      completed: false,
+      deadline: '2025-10-15',
+      niveau: 3,
+      importance: 5,
+    },
+    {
+      id: 8,
+      userId: 1,
+      title: 'Beispiel Todo 2',
+      completed: true,
+      deadline: '2025-11-20',
+      niveau: 2,
+      importance: 4,
+    },
+    {
+      id: 9,
+      userId: 1,
+      title: 'Beispiel Todo 3',
+      completed: true,
+      deadline: '2025-11-20',
+      niveau: 2,
+      importance: 4,
+    },
   ];
-  todos = signal<Array<Todo>>(JSON.parse(localStorage.getItem('todos') || '[]'));
+  todos = signal<Array<Todo>>(
+    JSON.parse(localStorage.getItem('todos') || '[]')
+  );
 
-
-  loadTodos(){
+  loadTodos() {
     /* if (this.todos().length === 0) {
       this.getTodosFromApi()
         .pipe(
@@ -44,29 +68,32 @@ export class TodoService {
     const url = '../assets/todos.json';
     console.log('Lade JSON von:', url);
     return this.http.get<Todo[]>(url);
-/*     return this.http.get<Array<Todo>>(url);
- */  }
+    /*     return this.http.get<Array<Todo>>(url);
+     */
+  }
 
   sortArrayTodosByCompletion(): Todo[] {
-    this.localTodos.sort((a, b) => (a.completed === b.completed) ? 0 : a.completed ? -1 : 1);
+    this.localTodos.sort((a, b) =>
+      a.completed === b.completed ? 0 : a.completed ? -1 : 1
+    );
     return this.localTodos;
   }
 
   markAsCompleted(id: number): Todo[] {
-    const todo = this.localTodos.find(todo => todo.id === id);
+    const todo = this.localTodos.find((todo) => todo.id === id);
     if (todo) {
       todo.completed = true;
     }
     return this.localTodos;
   }
 
-  addTodo(newTodo: Todo){
+  addTodo(newTodo: Todo) {
     this.localTodos.push(newTodo);
     localStorage.setItem('todos', JSON.stringify(this.localTodos));
   }
 
-  deleteTodo(id: number){
-    const index = this.localTodos.findIndex(todo => todo.id === id);
+  deleteTodo(id: number) {
+    const index = this.localTodos.findIndex((todo) => todo.id === id);
     if (index !== -1) {
       this.localTodos.splice(index, 1);
     }
@@ -74,7 +101,7 @@ export class TodoService {
     return this.localTodos;
   }
 
-  clearStorage(){
+  clearStorage() {
     //maybe to clear all the todos that are done?
   }
 }
--- Frontend/src/app/todos/todos.component.ts
+++ Frontend/src/app/todos/todos.component.ts
@@ -2,34 +2,36 @@ import { Component, inject, signal, effect, ViewChild } from '@angular/core';
 import { TodoService } from '../services/todo.service';
 import { Todo } from '../model/todo.type';
 import { map } from 'rxjs';
-import { TodoitemComponent } from "../components/todoitem/todoitem.component";
+import { TodoitemComponent } from '../components/todoitem/todoitem.component';
 import { HighlightDoneTodosDirective } from '../directives/highlight-done-todos.directive';
 import { PopupComponent } from '../components/popup/popup.component';
 import { NgFor } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import todoData from 'C:/Users/haaseja/Desktop/DHBW/Bericht Repo/ToDo-Website/Frontend/src/assets/todos.json'
+import todoData from '../../assets/todos.json';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [PopupComponent,CommonModule],
+  imports: [PopupComponent, CommonModule],
   templateUrl: './todos.component.html',
-  styleUrl: './todos.component.css'
+  styleUrl: './todos.component.css',
 })
 export class TodosComponent {
   @ViewChild(PopupComponent) popup!: PopupComponent;
 
-  todos = signal<Array<Todo>>(JSON.parse(localStorage.getItem('todos') || '[]'));
+  todos = signal<Array<Todo>>(
+    JSON.parse(localStorage.getItem('todos') || '[]')
+  );
   /*dustbin = signal<Array<Todo>>(JSON.parse(localStorage.getItem('dustbin') || '[]'));*/
-  todoService = inject(TodoService);  
+  todoService = inject(TodoService);
   arrayTodos: Todo[] = [];
 
-  openPopup(title:string, text: string) {
+  openPopup(title: string, text: string) {
     this.popup.open(title, text);
   }
 
-  openEdit(title:string, id: Todo){
+  openEdit(title: string, id: Todo) {
     this.popup.openEdit(title, id);
   }
 
@@ -39,23 +41,24 @@ export class TodosComponent {
   }
 
   ngOnInit() {
-    this.arrayTodos = this.todoService.loadTodos();   
+    this.arrayTodos = this.todoService.loadTodos();
   }
   sortedTodos() {
-    return this.todos().slice().sort((a, b) => Number(a.completed) - Number(b.completed));
+    return this.todos()
+      .slice()
+      .sort((a, b) => Number(a.completed) - Number(b.completed));
   }
   deleteTodo(todoId: number) {
-    this.todos.set(this.todos().filter(todo => todo.id !== todoId));
+    this.todos.set(this.todos().filter((todo) => todo.id !== todoId));
   }
 
-  deleteArrayTodo(todoID: number){
+  deleteArrayTodo(todoID: number) {
     this.todoService.deleteTodo(todoID);
   }
 
   markDoneArrayToDo(id: number) {
     this.todoService.markAsCompleted(id);
   }
-
 
   /*   constructor() {
       effect(() => {
@@ -65,11 +68,10 @@ export class TodosComponent {
 
   toggleCompleted(todo: Todo) {
     todo.completed = !todo.completed;
-    this.todos.set(this.todos().slice().sort((a, b) => Number(a.completed) - Number(b.completed)));
+    this.todos.set(
+      this.todos()
+        .slice()
+        .sort((a, b) => Number(a.completed) - Number(b.completed))
+    );
   }
-
-  
-
 }
-
-
