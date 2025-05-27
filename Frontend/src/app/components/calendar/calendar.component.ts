b96c09120a1d20ae16ebb90b5f09398b5620b96c
Jannik <Blitzcolt34@gmail.com>
2025-05-27 17:38:02 +0200

    calendar-toolbar

--- Frontend/src/app/components/calendar/calendar.component.ts
+++ Frontend/src/app/components/calendar/calendar.component.ts
@@ -1,6 +1,7 @@
 import { Component, AfterViewInit } from '@angular/core';
-import { Calendar } from '@fullcalendar/core';
+import { Calendar, CalendarOptions } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
 import { TodoService } from '../../services/todo.service';
 import { Todo } from '../../model/todo.type';
 
@@ -8,33 +9,40 @@ import { Todo } from '../../model/todo.type';
   selector: 'app-calendar',
   standalone: true,
   templateUrl: './calendar.component.html',
-  styleUrl: './calendar.component.css'
+  styleUrl: './calendar.component.css',
 })
 export class CalendarComponent implements AfterViewInit {
-
   todos: Todo[] = [];
   constructor(private todoService: TodoService) {
-    this.todos = this.todoService.loadTodos()
+    this.todos = this.todoService.loadTodos();
   }
 
   ngAfterViewInit() {
     const calendarEl = document.getElementById('calendar');
     if (calendarEl) {
       const calendar = new Calendar(calendarEl, {
-        plugins: [dayGridPlugin],
-        initialView: 'dayGridMonth',
+        plugins: [dayGridPlugin, timeGridPlugin],
+        headerToolbar: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay',
+        },
         events: [
-          ...this.todos.map(todo => ({
+          ...this.todos.map((todo) => ({
             title: todo.title,
             start: todo.deadline,
             allDay: true,
+            end: todo.deadline,
             color: todo.completed ? 'green' : 'red',
+            backgroundColor: todo.completed ? 'lightgreen' : 'lightcoral',
+            borderColor: todo.completed ? 'green' : 'red',
+            textColor: 'black',
             extendedProps: {
               niveau: todo.niveau,
-              importance: todo.importance
-            }
-          }))
-        ]
+              importance: todo.importance,
+            },
+          })),
+        ],
       });
       calendar.render();
     }
