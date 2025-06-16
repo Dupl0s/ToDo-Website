68213e92eee3b88e319b31a5b9f8eaf29380db7f
JojoArchiv <johanna.wolfstieg@posteo.de>
2025-06-16 14:49:40 +0200

    app.routes error fixed

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { TodosComponent } from './todos/todos.component';
 import { DustbinComponent } from './dustbin/dustbin.component';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
 
 
 export const routes: Routes = [/*{
@@ -13,6 +12,5 @@ export const routes: Routes = [/*{
 }*/
 {path: "todos", component: TodosComponent},
 { path: "dustbin", component: DustbinComponent },
-{ path: '', component: HomeComponent },
-{ path: "login", component: LoginComponent}
+{ path: '', component: HomeComponent }
 ];
