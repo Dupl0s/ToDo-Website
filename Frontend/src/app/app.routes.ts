4bef6fb7f31073330b72d940925ea64a50bffb41
Sribriddhi <sribriddhibanerjee@gmail.com>
2025-06-20 10:30:35 +0200

    Bereiche(Categories Creation)
    4 major things done:
    1. Put the To-Dos inside Bereiche
    2. CRUD Operation for the Bereiche
    3. Edit the header to display Bereiche(Categories)
    4. Edit the "To-Dos" in the header to not display editing options

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { TodosComponent } from './todos/todos.component';
 import { DustbinComponent } from './dustbin/dustbin.component';
 import { HomeComponent } from './home/home.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 
 export const routes: Routes = [/*{
@@ -10,7 +11,10 @@ export const routes: Routes = [/*{
         return import('./todos/todos.component').then((m) => m.TodosComponent)
     }
 }*/
+{path: "categories", component: CategoriesComponent},
+{path: "todos/:id", component: TodosComponent},
 {path: "todos", component: TodosComponent},
 { path: "dustbin", component: DustbinComponent },
+{ path: 'dustbin/:id', component: DustbinComponent },
 { path: '', component: HomeComponent }
 ];
