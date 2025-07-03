2c1dfd86b634f6b9f6b590481f6d1aebc558b6f9
Jannik <Blitzcolt34@gmail.com>
2025-07-03 21:25:09 +0200

    added get for sections and todos

--- Backend/src/db/schema.ts
+++ Backend/src/db/schema.ts
@@ -1,6 +1,25 @@
-import { pgTable, serial, text } from 'drizzle-orm/pg-core';
+import { pgTable, serial, text, uuid, boolean, integer } from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
   id: serial('id').primaryKey(),
   name: text('name').notNull(),
+  userid: uuid('userid'),
+  password: text('password'),
+  email: text('email'),
+});
+
+export const todos = pgTable('todos', {
+  id: serial('id').primaryKey(),
+  userid: uuid('userid'),
+  completed: boolean('completed'),
+  deadline: text('deadline'),
+  niveau: integer('niveau'),
+  importance: integer('importance'),
+  bereichsID: integer('bereichsID'),
+  title: text('title'),
+});
+
+export const sections = pgTable('sections', {
+  id: serial('id').notNull(),
+  name: text('name'),
 });
